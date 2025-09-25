949aacb850d5d54bf82c0d166fb21cbc3dfc97a2
gabe alme <gabealmeemail@gmail.com>
2025-09-24 18:02:33 -0700

    Added imports for working with files, reading in files, and determining if they are sudoku boards and what size they are.

--- Sudoku.java
+++ Sudoku.java
@@ -1,5 +1,27 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Sudoku {
-    
+
+    public int fileCheck(String fileName){
+        File file = new File(fileName);
+        try(Scanner fileRead = new Scanner(file)){
+            if(fileRead.hasNextLine()){
+                String boardSize = fileRead.nextLine();
+                if(boardSize.equals("9x9")){ return 9; }
+                if(boardSize.equals("4x4")){ return 4; }
+            }
+        } catch(FileNotFoundException e){
+            System.out.println("File not found, please input a new file name.")
+            return 0;
+        }
+
+
+        return 0;
+    }
+
+
     public static void main(String[] args) {
         System.out.println("Hello, Sudoku!");
     }
